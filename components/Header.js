32ee5d3597302e74ede3d7f2a4e98edeea3225b1
Nathan Hietala <Nrhietala@gmail.com>
2020-12-04 10:08:38 -0600

    Initial commit and setup

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,25 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+  const header = document.createElement('div')
+  header.classList.add('header');
+
+  const date = document.createElement('span')
+  date.classList.add('date');
+  date.textContent = 'March 28, 2020';
+  header.append(date);
+
+  const title = document.createElement('h1')
+  title.textContent = "Lambda Times";
+  header.append(title);
+
+  const temp = document.createElement('span')
+  temp.classList.add(temp)
+  temp.textContent = 98;
+  header.append(temp);
+
+  return header;
+}
+
+console.log("Heyo")
